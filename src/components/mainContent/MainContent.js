63598d11faa9a4d6d3aeeacb44a2a57e8a5b6a44
raphaeldonanu <raphael.donanu@konga.com>
2022-05-20 09:55:14 +0100

    fixing responsiveness

--- src/components/mainContent/MainContent.js
+++ src/components/mainContent/MainContent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './MainContent.module.scss';
 import Icon from '../../assets/icon';
+import { isMobile } from 'react-device-detect';
 
 const MainContent = () => {
   return (
@@ -8,13 +9,14 @@ const MainContent = () => {
             <div className={styles.content}>
                 <div className={styles.text}>
                     <div>
-                    {/* <div>
-                        <h1>Unlock to <span className={styles.bold}>Power your dreams!</span></h1>
-                    </div> */}
+                        {
+                            isMobile ? <h1>Unlock to <span className={styles.bold}>Power your dreams!</span></h1> : 
+                            <h1>Unlock to <br /><span className={styles.bold}>Power your dreams!</span></h1>
+                        }
+                        {isMobile ? <p>Stand a chance to win <span>N3,000,000 </span> everyday for the next 5 days</p> :
+                            <p>Stand a chance to win <br /><span>N3,000,000 </span> everyday for the next 5 days</p>
+                        }
                         
-                        {/* <div className={styles.center}>
-                            <p>Stand a chance to win <span>N3,000,000 </span> everyday for the next 5 days</p>
-                        </div> */}
                     </div>
                 </div>
             </div>
